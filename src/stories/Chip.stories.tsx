12047c52e7ea10ab37df43de4a9e7ed97b81737c
Roman Firsov <Rfirsov@users.noreply.github.com>
2025-03-05 13:47:07 +0200

    Extend Chip stories

--- src/stories/Chip.stories.tsx
+++ src/stories/Chip.stories.tsx
@@ -4,7 +4,7 @@ import { Icon } from '../../lib/components/Icons';
 import Chip from '../../lib/components/Chip';
 
 const meta: Meta<typeof Chip> = {
-  title: 'Design System/Input',
+  title: 'Design System/Chip',
   component: Chip,
   tags: ['autodocs'],
 };
@@ -19,5 +19,34 @@ export const Basic: Story = {
     chipState: 'success',
     variant: 'outlined',
     iconLeft: <Icon name="Star" />,
+  },
+  argTypes: {
+    size: {
+      options: ['medium', 'small', 'tiny'],
+      control: { type: 'select' },
+      description: 'The size of the chip',
+    },
+    variant: {
+      options: ['contained', 'outlined'],
+      control: { type: 'inline-radio' },
+      description: 'The visual style of the chip',
+    },
+    chipState: {
+      options: ['default', 'success', 'warning', 'error', 'info'],
+      control: { type: 'select' },
+      description: 'The state of the chip',
+    },
+    iconLeft: {
+      control: 'object',
+      description: 'The icon to display inside the chip',
+    },
+    onClose: {
+      action: 'clicked',
+      description: 'The click event handler of the close button',
+    },
+    customClassName: {
+      control: 'text',
+      description: 'Custom class name for the chip',
+    },
   },
 };
